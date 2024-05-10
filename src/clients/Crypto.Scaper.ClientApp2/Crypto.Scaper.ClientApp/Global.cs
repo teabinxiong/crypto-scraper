f8c666adab6b0a7e2ba959d33ab353ec6fef5d79
binxiong <binxiong.tea@redone.com.my>
2024-05-10 17:13:26 +0800

    Add another Client App project

---
+++ src/clients/Crypto.Scaper.ClientApp2/Crypto.Scaper.ClientApp/Global.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto.Scaper.ClientApp
+{
+	public class Global
+	{
+		public static List<ManualResetEvent> mre = new List<ManualResetEvent>();
+		public static bool IsStop = false;
+	}
+}
