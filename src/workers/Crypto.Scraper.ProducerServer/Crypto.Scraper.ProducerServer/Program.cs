9ae36fef3b9d1ca2c2de6fe28b786204c62d74eb
binxiong <binxiong.tea@redone.com.my>
2024-05-10 11:17:55 +0800

    Add Generic Host & Set up Logging

--- src/workers/Crypto.Scraper.ProducerServer/Crypto.Scraper.ProducerServer/Program.cs
+++ src/workers/Crypto.Scraper.ProducerServer/Crypto.Scraper.ProducerServer/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+var builder = new ConfigurationBuilder();
+builder.SetBasePath(Directory.GetCurrentDirectory())
+	.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+	.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+	.AddEnvironmentVariables();
+
+IHost host = Host.CreateDefaultBuilder()
+	.ConfigureServices((context, services) =>
+	{
+	})
+	.UseSerilog()
+	.Build();
+
+Log.Logger = new LoggerConfiguration()
+	.ReadFrom.Configuration(builder.Build())
+	//.Enrich.FromLogContext()
+	.MinimumLevel.Verbose()
+	.CreateLogger();
+
+host.Run();
