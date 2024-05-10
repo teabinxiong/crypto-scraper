629fc819063c7b2f3cce3e6e17041b558b12b672
binxiong <binxiong.tea@redone.com.my>
2024-05-10 12:25:17 +0800

    Add thread-safe collection

--- src/workers/Crypto.Scraper.ProducerServer/Crypto.Scraper.ProducerServer/Global.cs
+++ src/workers/Crypto.Scraper.ProducerServer/Crypto.Scraper.ProducerServer/Global.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Crypto.Scraper.ProducerServer.Models;
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +13,7 @@ namespace Crypto.Scraper.ProducerServer
 		public static Serilog.ILogger Logger;
 
 		public static List<ManualResetEvent> ThreadCompleteEvents = new List<ManualResetEvent>();
+
+		public static ConcurrentDictionary<string, BlockingCollection<Data>> DataQueue = new ConcurrentDictionary<string, BlockingCollection<Data>>();
 	}
 }
