488434442e1338cfbee668d9537de382700fce86
binxiong <binxiong.tea@redone.com.my>
2024-05-10 12:17:24 +0800

    Setup the background services

---
+++ src/workers/Crypto.Scraper.ProducerServer/Crypto.Scraper.ProducerServer/ApplicationServices/BackgroudService.cs
@@ -0,0 +1,35 @@
+﻿using Crypto.Scraper.ProducerServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto.Scraper.ProducerServer.ApplicationServices
+{
+	public sealed class BackgroudService
+	{
+		private ServicesManager _servicesManager;
+		public BackgroudService()
+		{
+			_servicesManager = new ServicesManager();
+		}
+
+		public void Start()
+		{
+			Global.Logger.Information("Start Data Processing Service");
+
+			_servicesManager.StartAllThread();
+
+			Global.Logger.Information("Press q or Q to quit");
+		}
+
+		public void Stop()
+		{
+			Console.WriteLine("Quit Data Processing Service");
+
+			Global.Logger.Information("Quit program!");
+			_servicesManager.StopAllThread();
+		}
+	}
+}
